e57dff148e1f112f015597d4fdbd7ffca4945e81
Hoko Le <hokole527@gmail.com>
2019-10-04 22:45:45 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -52,7 +52,7 @@ def checkout(cart, coupons)
   hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   appied_discount = apply_clearance(applied_coupons)
-  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value
+  total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price]
   
   # code here
 end
