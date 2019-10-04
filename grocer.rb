ffd40295e08c50c31050cb7b0257d4200ab166f5
Hoko Le <hokole527@gmail.com>
2019-10-04 22:53:07 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -48,8 +48,8 @@ def apply_clearance(cart)
   # code here
 end
 
-def checkout(cart, coupons)
-  hash_cart = consolidate_cart(cart)
+def checkout(array, coupons)
+  hash_cart = consolidate_cart(array)
   applied_coupons = apply_coupons(hash_cart, coupons)
   appied_discount = apply_clearance(applied_coupons)
   total = applied_discount.reduce(0) { |acc, (key, value)| acc += value[:price] * value[:count]}
