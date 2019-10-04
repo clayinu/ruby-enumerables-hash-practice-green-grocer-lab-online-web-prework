baff4f11052e3efda4c4b01272ab51e64161dc2c
Hoko Le <hokole527@gmail.com>
2019-10-04 22:43:57 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -50,7 +50,8 @@ end
 
 def checkout(cart, coupons)
   hash_cart = consolidate_cart(array)
-  applied_coupons = apply_coupons()
+  applied_coupons = apply_coupons(hash_cart, coupons)
+  
   
   # code here
 end
